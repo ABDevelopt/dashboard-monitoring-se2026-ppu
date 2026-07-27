bd876ffc97f9b6fa030bc8794b3c0c002a4229a8
Imam Dzulvan M <11231031@student.itk.ac.id>
2026-07-27 11:00:09 +0800

    fix: PCL detail card not showing any information

--- routes/api.js
+++ routes/api.js
@@ -217,11 +217,11 @@ router.get('/detail/pcl', (req, res) => {
       CASE WHEN p.kode IS NOT NULL AND (${targetMuatanFormula}) > 0 AND (${realFormula}) >= (${targetMuatanFormula}) THEN 1 ELSE 0 END AS sudah_diisi,
       (${usahaTotalFormula}) AS usaha_total,
       (${keluargaTotalFormula}) AS keluarga_total
-    FROM progres p
-    LEFT JOIN subsls_master m ON p.kode = m.kode
-    WHERE p.upload_id = ? AND (p.pcl_name = ? OR p.pcl_email = ?)
-    ORDER BY m.kecamatan, m.desa, p.kode
-  `).all(uploadId, name, name);
+    FROM subsls_master m
+    LEFT JOIN progres p ON m.kode = p.kode AND p.upload_id = ?
+    WHERE m.pcl = ? OR p.pcl_name = ? OR p.pcl_email = ?
+    ORDER BY m.kecamatan, m.desa, m.kode
+  `).all(uploadId, name, name, name);
 
   res.json(attachProgressPercentages(data));
 });
